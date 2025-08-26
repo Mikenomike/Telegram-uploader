8fe671702dddfeb445ab98815503615a13d9abcf
Mikenomike <plp627021@gmail.com>
2025-08-26 22:57:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello from Render!")
+from aiogram import Bot, Dispatcher, types
+import asyncio
+import os
+
+# توکن ربات رو از متغیر محیطی می‌گیریم
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("✅ Bot is on!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
